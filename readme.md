e09ce2cf1dc4cedacfbffe6fd75b996b9e00cb6a
kfinley <kyle@kylefinley.net>
2022-04-24 09:04:56 -0700

    Removed xunit and MSpec bits which was causing nuget package build to not be created

--- readme.md
+++ readme.md
@@ -5,13 +5,9 @@ SUT is a wrapper class for any .net class that you want to treat as a System Und
 
 ### Why would I use it?
 TODO
+
 ### How do I use it?
 TODO
-### Anything else I should know?
-Package includes an MSpec IClassFixture base class that can be used for writing MSpec based tests using SUT.
 
-#### Example of using SpecBase
-TODO
 ### Issues?
-
 If you have any issues please report them in the project's GitHub Issues.
--- src/Specs/SpecBase.cs
+++ src/Specs/SpecBase.cs
@@ -1,29 +1,29 @@
-using Machine.Specifications;
-using Xunit;
+// using Machine.Specifications;
+// using Xunit;
 
-namespace Sut.Specs {
+// namespace Sut.Specs {
 
-  // original source: https://github.com/kfinley/Platform8/blob/main/dotnet/Tests.Common/Specs/SpecBase.cs
-  public abstract class SpecBase : IClassFixture<MSpecFixture> {
-    private object lockObject = new object();
+//   // original source: https://github.com/kfinley/Platform8/blob/main/dotnet/Tests.Common/Specs/SpecBase.cs
+//   public abstract class SpecBase : IClassFixture<MSpecFixture> {
+//     private object lockObject = new object();
 
-    protected MSpecFixture Fixture;
+//     protected MSpecFixture Fixture;
 
-    public SpecBase(MSpecFixture fixture) {
-      Fixture = fixture;
-    }
+//     public SpecBase(MSpecFixture fixture) {
+//       Fixture = fixture;
+//     }
 
-    protected void Setup(IClassFixture<MSpecFixture> spec, Establish context, Because of) {
-      lock (lockObject)
-        Fixture.Setup(spec, context, of);
-    }
-  }
+//     protected void Setup(IClassFixture<MSpecFixture> spec, Establish context, Because of) {
+//       lock (lockObject)
+//         Fixture.Setup(spec, context, of);
+//     }
+//   }
 
-  public abstract class SpecBase<TRequest, TResponse> : SpecBase {
-    protected static TRequest Request;
-    protected static TResponse Result;
+//   public abstract class SpecBase<TRequest, TResponse> : SpecBase {
+//     protected static TRequest Request;
+//     protected static TResponse Result;
 
-    public SpecBase(MSpecFixture fixture) : base(fixture) {
-    }
-  }
-}
+//     public SpecBase(MSpecFixture fixture) : base(fixture) {
+//     }
+//   }
+// }
